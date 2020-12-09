3a5001820a860e21230c7cc1761f3e52a244b7c4
Rizqial5 <bagusriski1@gmail.com>
2020-12-09 14:10:27 +0700

    Guest sudah 99%

--- Guest.py
+++ Guest.py
@@ -42,15 +42,24 @@ class aGuest :
             self.cursor.execute(inputdata)
             self.myDb.commit()
             print("Terimakasih telah melakukan pembelian")
+        elif konfirm == 'tidak':
+            aGuest.keranjang.clear()
+            aGuest.harga.clear()
+            print("Isi keranjangmu sudah dihapus semua")
+            aGuest().menuguest()
+        else :
+            print("Masukkan command dengan benar")
+            aGuest().menuguest()
+
 
 
     def menuguest(self):
         menuGuest =int(input(''' 
             MENU Guest
             1. Mencari Barang
-            3. Membeli Barang
-            4. Daftar Member
-            5. Exit
+            2. Membeli Barang
+            3. Daftar Member
+            4. Exit
 
             Masukkan Pilihan Menu: 
              '''))
@@ -58,8 +67,14 @@ class aGuest :
             aGuest().cari()
         elif menuGuest == 2 :
             aGuest().beli()
+        elif menuGuest == 3 :
+            from insert import person #biar bisa langsung menjalankan fungsi person insert data class person harus dibedakan file pythonnya kalau tidak nanti manggil 2 kali
+            person().insertData()
+        elif menuGuest == 4 :
+            exit(0)
         else:
-            print("a")
+            print("Masukkan Command dengan benar")
+            aGuest().menuguest()
 
 
 
